629555606a65fd4a9e090f268d5a98d3cdbd1866
Ramesh <buddharamesh2000@gmail.com>
2023-04-06 04:34:17 +0000

    get first names

---
+++ names/index.js
@@ -0,0 +1,8 @@
+const names = require("../country/state/city/index.js");
+const getFirstNames = require("../utilities/utils/index.js");
+
+const getPeopleInCity = (names) => {
+  return getFirstNames(names);
+};
+
+module.exports = getPeopleInCity;
